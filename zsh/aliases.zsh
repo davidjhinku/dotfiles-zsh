29d36a8639f78fb66d3c05a551b0ff9b8f240119
David Jhinku <david.jhinku@sezzle.com>
2024-02-08 12:18:40 -0600

    added navigation

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,9 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+# Easier navigation: .., ..., ...., .....
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
